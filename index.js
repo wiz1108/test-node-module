a9bbc922a8da3e77adac61f9a6bfca3413e4d5a5
wiz1108 <wiz1108@hotmail.com>
2025-03-21 17:41:15 -0400

    Add Nftcard1

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import Icon from './components/Icon'
 import List from './components/List'
 import NavigationSwiper from './components/NavigationSwiper'
 import NftCard from './components/NftCard'
+import NftCard1 from './components/NftCard1'
 import Notification from './components/Notification'
 import Price from './components/Price'
 import Search from './components/Search'
@@ -25,6 +26,7 @@ module.exports = {
   List,
   NavigationSwiper,
   NftCard,
+  NftCard1,
   Notification,
   Price,
   Search
